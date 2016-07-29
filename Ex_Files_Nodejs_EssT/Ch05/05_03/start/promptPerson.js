2dc68ee586e83548c818d976032c9ad217c59402
mediaslug <github@mediaslug.com>
2016-07-29 13:22:26 -0500

    completed 05_03

--- Ex_Files_Nodejs_EssT/Ch05/05_03/start/promptPerson.js
+++ Ex_Files_Nodejs_EssT/Ch05/05_03/start/promptPerson.js
@@ -1,4 +1,6 @@
 var readline = require('readline');
+var fs = require("fs");
+
 var rl = readline.createInterface(process.stdin, process.stdout);
 
 var realPerson = {
@@ -11,6 +13,12 @@ rl.question("What is the name of a real person? ", function(answer) {
 
 	realPerson.name = answer;
 
+	// we're using Sync because  In this case, this application only has one user. It runs in the terminal,
+	// so we're not going to have a blocking request that will cause delays for other users. also since we are doing this
+	// asynchronously, there is no need to add a callback function
+
+	fs.writeFileSync(realPerson.name + ".md", `${realPerson.name}\n=======================\n\n`);
+
 	rl.setPrompt(`What would ${realPerson.name} say? `);
 
 	rl.prompt();
@@ -18,6 +26,11 @@ rl.question("What is the name of a real person? ", function(answer) {
 	rl.on('line', function(saying) {
 
 		realPerson.sayings.push(saying.trim());
+		// example given is asynchronously appending, but he mentions that it doesn't matter and
+		// also we do not include a call back function. it seems error handling would be good for a callback function though
+		// what if we can't append?
+
+		fs.appendFile(realPerson.name + ".md", `* ${saying.trim()}\n` ) // the * is for the markdown bullet point, not a special symbol in this code
 
 		if (saying.toLowerCase().trim() === 'exit') {
 			rl.close();
